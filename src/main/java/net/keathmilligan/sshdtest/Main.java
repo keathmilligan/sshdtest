8b9ec73fd316fae0aff16f1eb0a2a80b6d1bd1ee
Keath Milligan <keath@keathmilligan.net>
2019-08-18 13:22:00 -0500

    Cleanup, handle standard input from Gradle command line execution.

--- src/main/java/net/keathmilligan/sshdtest/Main.java
+++ src/main/java/net/keathmilligan/sshdtest/Main.java
@@ -3,9 +3,7 @@ package net.keathmilligan.sshdtest;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.sshd.server.SshServer;
-import org.apache.sshd.server.auth.password.PasswordAuthenticator;
 import org.apache.sshd.server.keyprovider.SimpleGeneratorHostKeyProvider;
-import org.apache.sshd.server.session.ServerSession;
 import org.apache.sshd.server.shell.ProcessShellFactory;
 
 import java.io.IOException;
@@ -16,20 +14,10 @@ public class Main {
     public static void main(String args[]) {
         logger.info("sshd example");
         var sshServer = SshServer.setUpDefaultServer();
+        sshServer.setHost("127.0.0.1");
         sshServer.setPort(2222);
-        sshServer.setPasswordAuthenticator(
-                new PasswordAuthenticator() {
-                    @Override
-                    public boolean authenticate(
-                            String username,
-                            String password,
-                            ServerSession session) {
-                        return "ssh".equals(username) && "secret".equals(password);
-                    }
-                });
         sshServer.setKeyPairProvider(new SimpleGeneratorHostKeyProvider());
         sshServer.setPasswordAuthenticator((username, password, session) -> {
-            System.out.println("authenticating username: " + username + " password: " + password);
             return true;
         });
         sshServer.setShellFactory(new ProcessShellFactory("/bin/sh", "-i", "-l"));
